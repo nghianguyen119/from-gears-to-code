7fc22d5a57b223fd074248bc06be8b431bde1ece
Nghia Nguyen <trongnghia.nguyen@outlook.com>
2023-04-16 02:41:29 +0200

    #apps/blog - fix call action

--- apps/blog/src/pages/index.tsx
+++ apps/blog/src/pages/index.tsx
@@ -23,9 +23,9 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
+            to="/docs/languages-cheat-sheet"
           >
-            Outline of my programming philosophy
+            Outline of my what I have learned
           </Link>
         </div>
       </div>
